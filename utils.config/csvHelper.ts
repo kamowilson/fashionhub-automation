adbd57ededf314117ba3654c99d57efc9f74a68d
Kwame <77013832+kamowilson@users.noreply.github.com>
2024-11-30 12:44:51 +0100

    small changes

--- utils.config/csvHelper.ts
+++
@@ -1,7 +0,0 @@
-import * as fs from 'fs';
-
-export const writeToCsv = async (data: any[], filename: string) => {
-    const header = Object.keys(data[0]).join(',') + '\n';
-    const rows = data.map(item => Object.values(item).join(',')).join('\n');
-    await fs.promises.writeFile(filename, header + rows);
-};
